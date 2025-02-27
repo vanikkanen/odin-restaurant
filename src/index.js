cab14cc1d1edc6b36a6be318e22b7ba6dfa8e538
Valtteri Nikkanen <valtteri.nikkanen@valmet.com>
2025-02-27 14:45:35 +0200

    Create component for homepage

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import "./styles.css"
+import { homePage } from "./components/home-page/home-page"
 
 console.log("Hello from JS!")
+
+const content = document.querySelector("#content")
+content.appendChild(homePage())
